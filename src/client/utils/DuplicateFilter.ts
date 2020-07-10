b34a04f12fa56b647f0e75afb6079cf62ceda419
Microwavekonijn <sjorsw@protonmail.ch>
2020-07-10 14:24:08 +0200

    When did I change this

--- src/client/utils/DuplicateFilter.ts
+++ src/client/utils/DuplicateFilter.ts
@@ -18,7 +18,7 @@ export default class DuplicateFilter implements EventStreamFilter {
      */
     public constructor({
                            decay = 2000,
-                           partitions = 1,
+                           partitions = 8,
                            ignore = ['GainExperience'],
                        } = {}) {
 
