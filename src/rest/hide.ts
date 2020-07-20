0efe20b5294799b40db359db929ce108ecac7262
Microwavekonijn <sjorsw@protonmail.ch>
2020-07-20 23:18:08 +0200

    Fix

--- src/rest/hide.ts
+++ src/rest/hide.ts
@@ -2,7 +2,7 @@ import { baseOperations, baseRequest } from './baseTypes';
 import { ApplyOperation } from './utils/Types';
 import { setParam } from './utils/Helpers';
 
-export default function <T, Q, O extends baseOperations, R>(request: baseRequest<Q, T, O, R>, field: string): ApplyOperation<'show' | 'hide', Q, T, O, R> {
+export default function <T, Q, O extends baseOperations, R>(request: baseRequest<Q, T, O, R>, fields: string[]): ApplyOperation<'show' | 'hide', Q, T, O, R> {
     // @ts-ignore
-    return setParam(request, 'c:hide', field);
+    return setParam(request, 'c:hide', fields.join(','));
 }
