85f1d7bd0fbfbeae3d6740a61f4d4af98ceaceae
Microwavekonijn <sjorsw@protonmail.ch>
2020-07-24 18:29:01 +0200

    That is better

--- src/rest/types/reward.ts
+++ src/rest/types/reward.ts
@@ -1,13 +1,13 @@
 export default reward;
 
 type reward = {
-    'reward_id': string,
-    'reward_type_id': string,
-    'count_min': string,
-    'count_max': string,
-    'param1': string,
-    'param2': string,
-    'param3': string,
-    'param4': string,
-    'param5': string
+    reward_id: string,
+    reward_type_id: string,
+    count_min: string,
+    count_max: string,
+    param1: string,
+    param2: string,
+    param3: string,
+    param4: string,
+    param5: string
 };
--- src/rest/types/skill.ts
+++ src/rest/types/skill.ts
@@ -1,16 +1,16 @@
-import { lang } from '../utils/Types';
+import { lang } from '../utils/Types;
 
 export default skill;
 
 type skill = {
-    'skill_id': string,
-    'skill_line_id': string,
-    'skill_line_index': string,
-    'skill_points': string,
-    'grant_item_id': string,
-    'name': lang,
-    'description': lang,
-    'image_set_id': string,
-    'image_id': string,
-    'image_path': string
+    skill_id: string,
+    skill_line_id: string,
+    skill_line_index: string,
+    skill_points: string,
+    grant_item_id: string,
+    name: lang,
+    description: lang,
+    image_set_id: string,
+    image_id: string,
+    image_path': string
 };
