4d0e242007c581b3939756e645359ed1c2e501d6
Microwavekonijn <sjorsw@protonmail.ch>
2020-09-25 17:48:45 +0200

    Fix

--- src/client/managers/RestManager.ts
+++ src/client/managers/RestManager.ts
@@ -1,6 +1,5 @@
 import { censusRequest, collections } from '../../rest/utils/requestTypes';
 import collectionIndex from '../../rest/indexes/collectionIndex';
-import limit from '../../rest/commands/limit';
 import queryIndex from '../../rest/indexes/queryIndex';
 import Client from '../Client';
 import Cache from '../concerns/Cache';
@@ -8,14 +7,10 @@ import { Events } from '../utils/Constants';
 import { hasLimit, hasLimitPerDB } from '../../rest/utils/commandHelpers';
 
 export default abstract class RestManager<C extends collections> {
-    private static readonly label = 'RestManager';
-
-    protected readonly request: censusRequest<C>;
-
     protected constructor(
         protected readonly client: Client,
         public readonly cache: Cache,
-        request: censusRequest<C>,
+        protected readonly request: censusRequest<C>,
     ) {
         if ('c:tree' in request.params || 'c:distinct' in request.params)
             throw new Error(`Request object request will return unsupported responses(i.e. tree, distinct)`);
@@ -37,7 +32,7 @@ export default abstract class RestManager<C extends collections> {
     private async makeRequest(id: string) {
         const query = this.query(id);
 
-        this.client.emit(Events.DEBUG, `Fetching data using query ${JSON.stringify(query)}(${this.constructor.name}).`, RestManager.label);
+        this.client.emit(Events.DEBUG, `Fetching data using query ${JSON.stringify(query)}.`, this.constructor.name);
 
         const data = await this.client.get(this.request, query);
 
