81bd990f010e0854cb513edc74d243438a0d8454
Microwavekonijn <sjorsw@protonmail.ch>
2021-07-31 14:06:09 +0200

    Added zone_id plus computed definition and instance ids.

--- src/client/events/metagame.event.ts
+++ src/client/events/metagame.event.ts
@@ -1,5 +1,5 @@
-import { PS2Event } from './ps2.event';
-import { Events } from '../constants/client.constants';
+import {PS2Event} from './ps2.event';
+import {Events} from '../constants/client.constants';
 
 export class MetagameEvent extends PS2Event {
     readonly emit = Events.PS2_META_EVENT;
@@ -13,6 +13,15 @@ export class MetagameEvent extends PS2Event {
     readonly metagame_event_state: string;
     readonly metagame_event_state_name: string;
     readonly instance_id: string;
+    readonly zone_id: string;
+
+    get zoneDefinitionId(): number {
+        return Number.parseInt(this.zone_id, 10) & 0x0000FFFF;
+    }
+
+    get zoneInstanceId(): number {
+        return (Number.parseInt(this.zone_id, 10) & 0xFFFF0000) >> 16;
+    }
 
     toHash(): string {
         return `MetagameEvent:${this.world_id}:${this.timestamp}`;
