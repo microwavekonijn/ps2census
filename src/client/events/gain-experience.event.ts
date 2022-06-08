f5a888e51b4c9b80f791a9d5844874b22032dced
Sjors <15579298+microwavekonijn@users.noreply.github.com>
2022-06-08 19:21:24 +0200

    feat(client): allow the fetching of other_id character on gainexperience (#71)

--- src/client/events/gain-experience.event.ts
+++ src/client/events/gain-experience.event.ts
@@ -10,6 +10,16 @@ export class GainExperience extends CharacterEvent {
   readonly other_id: string;
   readonly zone_id: string;
 
+  /**
+   * Fetch the character data of other if any
+   *
+   * @return {Promise<any | undefined>}
+   */
+  async other(): Promise<any | undefined> {
+    if (this.other_id.length == 19)
+      return this.client.characterManager.fetch(this.character_id);
+  }
+
   toHash(): string {
     return `GainExperience:${this.character_id}:${this.timestamp}`;
   }
